5dfee50d4394ca54accd8715afe6a3009f17cece
Avraham Ben Aram <avrahambenaramsouza@gmail.com>
2024-04-21 00:02:39 -0300

    feat: static dir request

--- main.go
+++ main.go
@@ -3,25 +3,12 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"os"
 
 	"github.com/avrahambenaram/multiplayer-go/internal/config"
 )
 
-func index(w http.ResponseWriter, r *http.Request) {
-  header := w.Header()
-  header.Set("Content-Type", "text/html")
-
-  file, fileErr := os.ReadFile("./public/index.html")
-  if fileErr != nil {
-    w.WriteHeader(500)
-    w.Write([]byte("An error occurred"))
-    return
-  }
-  w.Write(file)
-}
 func main() {
-  http.HandleFunc("/", index)
+  http.Handle("/", http.FileServer(http.Dir("./public")))
 
   fmt.Printf("Server running on port %d\n", config.Port)
   http.ListenAndServe(fmt.Sprintf(":%d", config.Port), nil)
